3c9e099a6125ab48bc4b10abc8ef58e40516b324
Brian Reece <bdreece@mtu.edu>
2022-07-04 23:45:18 -0400

    Implemented shoelace formula for abstract polygon

---
+++ Coords.cs
@@ -0,0 +1,10 @@
+public readonly struct Coords
+{
+    public Coords(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double X { get; init; }
+    public double Y { get; init; }
+}
--- Polygon.cs
+++ Polygon.cs
@@ -1,22 +1,64 @@
 public class Polygon : Shape
 {
+    private List<Coords> coords;
     public Polygon(string[] fields)
     {
         id = int.Parse(fields[0]);
+        coords = new List<Coords>();
+        for (var i = 3; i <= 35; i += 4)
+        {
+            if (fields[i] == "" || fields[i + 2] == "") break;
+            var x = double.Parse(fields[i]);
+            var y = double.Parse(fields[i + 2]);
+            coords.Add(new(x, y));
+        }
     }
 
     public override double GetArea()
     {
-        return 0f;
+        var area = 0d;
+        var size = coords.Count;
+        for (var i = 0; i < size - 1; i++)
+        {
+            area += determinant(coords[i], coords[i + 1]);
+        }
+        area += determinant(coords[size - 1], coords[0]);
+        return area / 2;
     }
 
     public override double GetPerimeter()
     {
-        return 0f;
+        var perimeter = 0d;
+        var size = coords.Count;
+
+        for (var i = 0; i < size - 1; i++)
+        {
+            perimeter += euclideanDistance(coords[i], coords[i + 1]);
+        }
+        perimeter += determinant(coords[size - 1], coords[0]);
+        return perimeter;
+
     }
 
     public override (double, double) GetCentroid()
     {
-        return (0f, 0f);
+        var (xAvg, yAvg) = (0d, 0d);
+        var size = coords.Count;
+        foreach (var coord in coords)
+        {
+            xAvg += coord.X;
+            yAvg += coord.Y;
+        }
+        return (xAvg / size, yAvg / size);
+    }
+
+    private double determinant(Coords a, Coords b)
+    {
+        return (a.X * b.Y) - (b.X * a.Y);
+    }
+
+    private double euclideanDistance(Coords a, Coords b)
+    {
+        return Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
     }
 }
