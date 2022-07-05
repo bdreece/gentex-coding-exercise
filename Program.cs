b0dc46c45de4479ac1546b56279313d51f8df917
Brian Reece <bdreece@mtu.edu>
2022-07-04 23:46:25 -0400

    Working on asynchonous execution sequence

--- Program.cs
+++ Program.cs
@@ -1,37 +1,47 @@
 ﻿namespace gentex
 {
-    using System.Collections;
     class Program
     {
         static async Task Main(string[] args) => await new Program(args[1], args[2]).MainAsync();
-        private string inputFile, outputFile;
+        private StreamReader reader;
+        private StreamWriter writer;
         private ShapeFactory factory;
+        private List<Task<string>> tasks;
         public Program(string inputFile, string outputFile)
         {
-            this.inputFile = inputFile;
-            this.outputFile = outputFile;
+            Console.WriteLine("Input File: {0}", inputFile);
+            Console.WriteLine("Output File: {0}", outputFile);
             factory = new ShapeFactory();
+            reader = new StreamReader(inputFile);
+            writer = new StreamWriter(outputFile);
+            tasks = new List<Task<string>>();
         }
 
         public async Task MainAsync()
         {
-            var shapes = new ArrayList();
-            using (var reader = new StreamReader(inputFile))
+            while (true)
             {
-                string? line;
-                while ((line = await reader.ReadLineAsync()) != null)
-                {
-                    shapes.Add(factory.CreateShape(line));
-                }
+                var line = await reader.ReadLineAsync();
+                if (line == null) break;
+                tasks.Add(Transform(line));
             }
+            string[] outputs = await Task.WhenAll(tasks);
+            foreach (string output in outputs)
+            {
+                writer.WriteLine(output);
+                Console.WriteLine("Output Line: {0}", output);
+            }
+        }
 
-            using (var writer = new StreamWriter(outputFile))
+        public async Task<string> Transform(string line)
+        {
+            return await Task.Run(() =>
             {
-                foreach (Shape shape in shapes)
-                {
-                    await writer.WriteLineAsync(shape.IntoCsv());
-                }
-            }
+                Console.WriteLine("Input Line: {0}", line);
+                var shape = factory.CreateShape(line);
+                var output = shape.IntoCsv();
+                return output;
+            });
         }
     }
 }
